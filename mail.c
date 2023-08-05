cde76429af9676c427d894d474394e36fbc3fc74
sherlly <jacob@sherllymail.com>
2023-08-05 17:58:34 +0100

    added golf functionality

--- mail.c
+++ mail.c
@@ -8,57 +8,58 @@
 #include "config.h"
 
 
-#define fact_file "facts_store.txt"
-#define comps_file "compliments_store.txt"
+#define fact_file "text_sources/facts_store.txt"
+#define comps_file "text_sources/compliments_store.txt"
+#define golf_file "text_sources/golf_facts.txt"
 
 
 char* read_attachment(char* file_path) {
-	char * buffer = 0;
-	long length;
-	FILE * f = fopen(file_path, "rb");
+    char * buffer = 0;
+    long length;
+    FILE * f = fopen(file_path, "rb");
 
-	if (f)
-	{
-	  fseek (f, 0, SEEK_END);
-	  length = ftell (f);
-	  fseek (f, 0, SEEK_SET);
-	  buffer = malloc (length*2);
-	  if (buffer)
-	  {
-	    fread (buffer, 1, length, f);
-	  }
-	  fclose (f);
-	}
+    if (f)
+    {
+        fseek (f, 0, SEEK_END);
+        length = ftell (f);
+        fseek (f, 0, SEEK_SET);
+        buffer = malloc (length*2);
+        if (buffer)
+        {
+            fread (buffer, 1, length, f);
+        }
+        fclose (f);
+    }
 
-	if (buffer)
-	{
-		buffer[length] = '\0';
-		return buffer;
-	}
-	return NULL;
+    if (buffer)
+    {
+        buffer[length] = '\0';
+        return buffer;
+    }
+    return NULL;
 
 
 }
 
 int count_lines_of_file(char* file_path) {
-	FILE * fp;
-	int lines = 0;
-	char ch;
-	if ((fp = fopen(file_path, "r")) == NULL) {
-		fprintf(stderr,"Error! opening file");
-		exit(0);
-	    }
+    FILE * fp;
+    int lines = 0;
+    char ch;
+    if ((fp = fopen(file_path, "r")) == NULL) {
+        fprintf(stderr,"Error! opening file");
+        exit(0);
+    }
 
-	while(!feof(fp))
-	{
-	  ch = fgetc(fp);
-	  if(ch == '\n')
-	  {
-	    lines++;
-	  }
-	}
+    while(!feof(fp))
+    {
+        ch = fgetc(fp);
+        if(ch == '\n')
+        {
+            lines++;
+        }
+    }
 
-	return lines;
+    return lines;
 
 }
 
@@ -76,13 +77,13 @@ void get_random_word(char* file,char* buffer) {
         exit(0);
     }
     while (fgets(current, sizeof(current), words)) {
-	strcpy(selected, current);
-	if (chosen_line == lineno) {
-		break;
-	}
-	++lineno;
+        strcpy(selected, current);
+        if (chosen_line == lineno) {
+            break;
+        }
+        ++lineno;
 
-	}
+    }
     fclose(words);
     selectlen = strlen(selected);
     if (selectlen > 0 && selected[selectlen-1] == '\n') {
@@ -93,34 +94,34 @@ void get_random_word(char* file,char* buffer) {
 }
 
 struct upload_status {
-  size_t bytes_read;
+    size_t bytes_read;
 };
 
 char payload_text[FULL_PAYLOAD_SIZE];
 
 
 static size_t payload_source(char *ptr,size_t size,size_t nmemb,void *userp) {
-struct upload_status *upload_ctx = (struct upload_status *)userp;
-const char *data;
-  size_t room = size * nmemb;
+    struct upload_status *upload_ctx = (struct upload_status *)userp;
+    const char *data;
+    size_t room = size * nmemb;
 
-  if((size == 0) || (nmemb == 0) || ((size*nmemb) < 1)) {
+    if((size == 0) || (nmemb == 0) || ((size*nmemb) < 1)) {
+        return 0;
+    }
+
+    data = &payload_text[upload_ctx->bytes_read];
+
+    if(data) {
+        size_t len = strlen(data);
+        if(room < len)
+            len = room;
+        memcpy(ptr, data, len);
+        upload_ctx->bytes_read += len;
+
+        return len;
+    }
+
     return 0;
-  }
-
-  data = &payload_text[upload_ctx->bytes_read];
-
-  if(data) {
-    size_t len = strlen(data);
-    if(room < len)
-      len = room;
-    memcpy(ptr, data, len);
-    upload_ctx->bytes_read += len;
-
-    return len;
-  }
-
-  return 0;
 
 }
 
@@ -128,74 +129,74 @@ const char *data;
 
 void send_email(struct Email email) {
 
-	char to_email[SIZE];
-	char to[SIZE];
-	char subject[SUBJECT_SIZE];
-	char body[BODY_SIZE];
-	char attachment_content[ATTACHMENT_SIZE];
-	char* attachment_file;
-	char* attachment_file_name;
+    char to_email[SIZE];
+    char to[SIZE];
+    char subject[SUBJECT_SIZE];
+    char body[BODY_SIZE];
+    char attachment_content[ATTACHMENT_SIZE];
+    char* attachment_file;
+    char* attachment_file_name;
 
-	sprintf(to_email,"<%s>",email.To_addr);
-	sprintf(to,"%s <%s>",email.To_name,email.To_addr);
+    sprintf(to_email,"<%s>",email.To_addr);
+    sprintf(to,"%s <%s>",email.To_name,email.To_addr);
 
-	if (strlen(email.Attachment_name) > 0 && strlen(email.Attachment_path) > 0) {
+    if (strlen(email.Attachment_name) > 0 && strlen(email.Attachment_path) > 0) {
 
-	char* attachment_buffer = read_attachment(email.Attachment_path);
-	strncpy(attachment_content,attachment_buffer,ATTACHMENT_SIZE);
+        char* attachment_buffer = read_attachment(email.Attachment_path);
+        strncpy(attachment_content,attachment_buffer,ATTACHMENT_SIZE);
 
-	sprintf(payload_text,"To: %s \r\nFrom:%s \r\nCc: %s \r\nMIME-Version: 1.0\r\nContent-Type: multipart/mixed;\r\n\tboundary=\"XXXXboundary text\"\r\nSubject: %s \r\n\r\n--XXXXboundary text\r\nContent-Type: text/plain\r\n\r\n %s \r\n\r\n--XXXXboundary text\r\nContent-Type: text/plain;\r\nContent-Disposition: attachment;\r\n\tfilename=\"%s\"\r\n\r\n %s \r\n\r\n--XXXXboundary text--\r\n",to,from,email.Cc_addr,email.Subject,email.Body,email.Attachment_name,attachment_content);
-	}
-	else {
-	sprintf(payload_text,"To: %s \r\nFrom: %s \r\nCc: %s \r\nSubject: %s \r\n\r\n %s \r\n\r\n \r\n ",to,from,email.Cc_addr,email.Subject,email.Body);
-	}
+        sprintf(payload_text,"To: %s \r\nFrom:%s \r\nCc: %s \r\nMIME-Version: 1.0\r\nContent-Type: multipart/mixed;\r\n\tboundary=\"XXXXboundary text\"\r\nSubject: %s \r\n\r\n--XXXXboundary text\r\nContent-Type: text/plain\r\n\r\n %s \r\n\r\n--XXXXboundary text\r\nContent-Type: text/plain;\r\nContent-Disposition: attachment;\r\n\tfilename=\"%s\"\r\n\r\n %s \r\n\r\n--XXXXboundary text--\r\n",to,from,email.Cc_addr,email.Subject,email.Body,email.Attachment_name,attachment_content);
+    }
+    else {
+        sprintf(payload_text,"To: %s \r\nFrom: %s \r\nCc: %s \r\nSubject: %s \r\n\r\n %s \r\n\r\n \r\n ",to,from,email.Cc_addr,email.Subject,email.Body);
+    }
 
-	CURL *curl;
-	CURLcode res_ = CURLE_OK;
-	struct curl_slist *recipients = NULL;
-	struct upload_status upload_ctx = { 0 };
+    CURL *curl;
+    CURLcode res_ = CURLE_OK;
+    struct curl_slist *recipients = NULL;
+    struct upload_status upload_ctx = { 0 };
 
-	curl = curl_easy_init();
-	  if(curl) {
-	    char* EMAIL_USER = getenv("EMAIL_USER");
-	    char* EMAIL_PASS = getenv("EMAIL_PASS");
-	    char* EMAIL_SMTP = getenv("EMAIL_SMTP");
-	    if (EMAIL_USER == NULL || EMAIL_PASS == NULL || EMAIL_SMTP == NULL) {
-		    fprintf(stderr,"Could not find env variables\n");
-		    exit(0);
-	    }
-	    curl_easy_setopt(curl, CURLOPT_USERNAME, EMAIL_USER);
-	    curl_easy_setopt(curl, CURLOPT_PASSWORD, EMAIL_PASS);
+    curl = curl_easy_init();
+    if(curl) {
+        char* EMAIL_USER = getenv("EMAIL_USER");
+        char* EMAIL_PASS = getenv("EMAIL_PASS");
+        char* EMAIL_SMTP = getenv("EMAIL_SMTP");
+        if (EMAIL_USER == NULL || EMAIL_PASS == NULL || EMAIL_SMTP == NULL) {
+            fprintf(stderr,"Could not find env variables\n");
+            exit(0);
+        }
+        curl_easy_setopt(curl, CURLOPT_USERNAME, EMAIL_USER);
+        curl_easy_setopt(curl, CURLOPT_PASSWORD, EMAIL_PASS);
 
-	    curl_easy_setopt(curl, CURLOPT_URL, EMAIL_SMTP);
+        curl_easy_setopt(curl, CURLOPT_URL, EMAIL_SMTP);
 
-	    curl_easy_setopt(curl,CURLOPT_USE_SSL,(long)CURLUSESSL_ALL);
+        curl_easy_setopt(curl,CURLOPT_USE_SSL,(long)CURLUSESSL_ALL);
 
-	    curl_easy_setopt(curl, CURLOPT_MAIL_FROM, from_mail);
+        curl_easy_setopt(curl, CURLOPT_MAIL_FROM, from_mail);
 
-	    recipients = curl_slist_append(recipients, to_email);
-	    recipients = curl_slist_append(recipients, Cc_addr);
-	    curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
-	    curl_easy_setopt(curl, CURLOPT_READFUNCTION, payload_source);
-	    curl_easy_setopt(curl, CURLOPT_READDATA, &upload_ctx);
-	    curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
-	     /* Send the message */
-	    res_ = curl_easy_perform(curl);
+        recipients = curl_slist_append(recipients, to_email);
+        recipients = curl_slist_append(recipients, Cc_addr);
+        curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
+        curl_easy_setopt(curl, CURLOPT_READFUNCTION, payload_source);
+        curl_easy_setopt(curl, CURLOPT_READDATA, &upload_ctx);
+        curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+        /* Send the message */
+        res_ = curl_easy_perform(curl);
 
-	    /* Check for errors */
-	    if(res_ != CURLE_OK) {
-	      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-		      curl_easy_strerror(res_));
-	    }
-	    else {
-	    	printf("\nsent email - email to -->  %s with email: %s\n",email.To_name,email.To_addr);
-	    }
+        /* Check for errors */
+        if(res_ != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(res_));
+        }
+        else {
+            printf("\nsent email - email to -->  %s with email: %s\n",email.To_name,email.To_addr);
+        }
 
-	    /* Free the list of recipients */
-	    curl_slist_free_all(recipients);
+        /* Free the list of recipients */
+        curl_slist_free_all(recipients);
 
-	    curl_easy_cleanup(curl);
-	  }
+        curl_easy_cleanup(curl);
+    }
 }
 
 
@@ -207,86 +208,96 @@ void send_email(struct Email email) {
 
 
 void send_email_to_subs(struct Email_Sub* email_sub_array, int amount_of_subs) {
-	srand(time(NULL));
-	char* all = "\r\nThis is version two of my mailing app. Please contact me at jacob@sherllymail.com if there are any issues\r\n";
+    srand(time(NULL));
+    char* all = "\r\nSherlly's emailing app\r\n";
 
-	/* choose fact */
-	char fact[SELECTION_SIZE];
-	get_random_word(fact_file,fact);
+    /* choose fact */
+    char fact[SELECTION_SIZE];
+    get_random_word(fact_file,fact);
 
-	/* choose compliment */
-	char comp[SELECTION_SIZE];
-	get_random_word(comps_file,comp);
+    /* choose compliment */
+    char comp[SELECTION_SIZE];
+    get_random_word(comps_file,comp);
 
-	for (int i=0; i<amount_of_subs;i++) {
-		/* init vars*/
-		char subject[SUBJECT_SIZE];
-		char body[BODY_SIZE];
-		char* attachment_name = NULL;
-		char* attachment_path = NULL;
-		struct Email_Sub email_sub = email_sub_array[i];
-		struct Email email_template;
+    /* choose golf fact */
+    char golf_fact[SELECTION_SIZE];
+    get_random_word(golf_file,golf_fact);
 
-
-		sprintf(body,"%s",all);
-
-		/* Changed based on template */
-		if (strncmp("fact_app",email_sub.Temp,9) == 0) {
-			/* fact Template */
-
-			sprintf(subject,"Sherlly's Fact app for %s\n",email_sub.FirstName);
-			sprintf(body,"Hey! hope you enjoy todays fact:\n %s \n %s",fact,email_sub.AdditionalText);
-		}
-		else if (strncmp("comp_app",email_sub.Temp,9) == 0) {
-
-			/* compliment Template */
-			sprintf(subject,"Sherlly's Compliment app for %s\n",email_sub.FirstName);
-			sprintf(body,"\nHey! hope you have a good day queen:\n %s \n %s",comp,email_sub.AdditionalText);
-		}
-		else if (strncmp("mem_app",email_sub.Temp,9) == 0) {
-			/* Memory Template */
-			attachment_path = "attachments/memory.csv";
-			attachment_name = "memory_system.csv";
-			sprintf(subject,"Sherlly's Memory app for %s\n",email_sub.FirstName);
-			int rand_int = rand() % 10;
-
-			sprintf(body,"\nHey! Today you have to study:\n %d00s \n %s",rand_int,email_sub.AdditionalText);
+    for (int i=0; i<amount_of_subs;i++) {
+        /* init vars*/
+        char subject[SUBJECT_SIZE];
+        char body[BODY_SIZE];
+        char* attachment_name = NULL;
+        char* attachment_path = NULL;
+        struct Email_Sub email_sub = email_sub_array[i];
+        struct Email email_template;
 
 
-		}
-		else {
-		/* Default */
-			sprintf(subject,"Error finding email sub type for %s",email_sub.FirstName);
-			sprintf(body,"Please contact jacob@sherllymail for help");
+        sprintf(body,"%s",all);
 
-		}
+        /* Changed based on template */
+        if (strncmp("fact_app",email_sub.Temp,9) == 0) {
+            /* fact Template */
 
-		char tmp_to_name[SIZE];
-		sprintf(tmp_to_name,"%s %s", email_sub.FirstName,email_sub.LastName);
-		strncpy(email_template.To_addr,email_sub.Email,SIZE);
-		strncpy(email_template.To_name,tmp_to_name,SIZE);
-		strncpy(email_template.Cc_addr,Cc_addr,SIZE);
-		strncpy(email_template.Subject,subject,SUBJECT_SIZE);
-		strncpy(email_template.Body,body,BODY_SIZE);
-		if (attachment_path && attachment_name) {
-			strncpy(email_template.Attachment_path,attachment_path,SIZE);
-			strncpy(email_template.Attachment_name,attachment_name,SIZE);
-		}
-		else {
-			email_template.Attachment_path[0] = (char)0;
-			email_template.Attachment_name[0] = (char)0;
-		}
+            sprintf(subject,"Sherlly's Fact app for %s\n",email_sub.FirstName);
+            sprintf(body,"Hey! hope you enjoy todays fact:\n %s \n %s",fact,email_sub.AdditionalText);
+        }
+        else if (strncmp("comp_app",email_sub.Temp,9) == 0) {
 
-		send_email(email_template);
+            /* compliment Template */
+            sprintf(subject,"Sherlly's Compliment app for %s\n",email_sub.FirstName);
+            sprintf(body,"\nHey! hope you have a good day queen:\n %s \n %s",comp,email_sub.AdditionalText);
+        }
+        else if (strncmp("golf_app",email_sub.Temp,9) == 0) {
 
-	}
+            /* golf Template */
+            sprintf(subject,"Sherlly's Golf Fact app for %s\n",email_sub.FirstName);
+            sprintf(body,"\nHey! hope you enjoyed this golf factoid\n %s \n %s",comp,email_sub.AdditionalText);
+        }
+        else if (strncmp("mem_app",email_sub.Temp,9) == 0) {
+            /* Memory Template */
+            attachment_path = "attachments/memory.csv";
+            attachment_name = "memory_system.csv";
+            sprintf(subject,"Sherlly's Memory app for %s\n",email_sub.FirstName);
+            int rand_int = rand() % 10;
+
+            sprintf(body,"\nHey! Today you have to study:\n %d00s \n %s",rand_int,email_sub.AdditionalText);
+
+
+        }
+        else {
+            /* Default */
+            sprintf(subject,"Error finding email sub type for %s",email_sub.FirstName);
+            sprintf(body,"Please contact jacob@sherllymail for help");
+
+        }
+
+        char tmp_to_name[SIZE];
+        sprintf(tmp_to_name,"%s %s", email_sub.FirstName,email_sub.LastName);
+        strncpy(email_template.To_addr,email_sub.Email,SIZE);
+        strncpy(email_template.To_name,tmp_to_name,SIZE);
+        strncpy(email_template.Cc_addr,Cc_addr,SIZE);
+        strncpy(email_template.Subject,subject,SUBJECT_SIZE);
+        strncpy(email_template.Body,body,BODY_SIZE);
+        if (attachment_path && attachment_name) {
+            strncpy(email_template.Attachment_path,attachment_path,SIZE);
+            strncpy(email_template.Attachment_name,attachment_name,SIZE);
+        }
+        else {
+            email_template.Attachment_path[0] = (char)0;
+            email_template.Attachment_name[0] = (char)0;
+        }
+
+        send_email(email_template);
+
+    }
 
 
 
-	/* free the boi */
-	if (email_sub_array) {
-		free(email_sub_array);
-	}
+    /* free the boi */
+    if (email_sub_array) {
+        free(email_sub_array);
+    }
 
 }
 
